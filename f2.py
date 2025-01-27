b15529703cb31e66fc1527c0bf3aa0f63573bf86
RamitJ <rjoshi_be23@thapar.edu>
2025-01-27 15:24:04 +0530

    Delete f2.py

--- f2.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-df=pd.read_csv('/Users/yashagarwal/Downloads/archive/Iris.csv')
-
-# print(df.head())
-
-# print(df.drop(df.columns[[1,4]],axis=1))
-
-# print(df.drop(df.columns[[1,4]],axis=1,inplace=True))
-
