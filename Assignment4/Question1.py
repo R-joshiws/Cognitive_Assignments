0f7319f234e8050ca4beb384964109e092b7d042
RamitJ <rjoshi_be23@thapar.edu>
2025-02-03 12:06:17 +0530

    Add files via upload

---
+++ Assignment4/Question1.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+arr = np.array([10, 20, 30, 40, 50])
+# i.
+result = arr[0]+arr[2]
+print(result)
+# ii.
+multiplyResult= 3*arr
+print(multiplyResult)
+# iii.
+divideResult=arr/2
+print(divideResult)
